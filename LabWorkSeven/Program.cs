e1e23f68ec5f667e651ed46457dc0c2ef64c7d1e
ssplant <76212491+ssplant@users.noreply.github.com>
2021-01-09 16:07:03 +0900

    Оценки хранить в виде числа а не строки, добавить переменную-счетчик, которая будет хранить количество введенных студентов.

--- LabWorkSeven/Program.cs
+++ LabWorkSeven/Program.cs
@@ -7,28 +7,20 @@ namespace LabWorkSeven
         static void Main(string[] args)
         {
             Student[] students = new Student[10];
-            Student.NewStudent(students);
-
+            var count = Student.NewStudent(students);
             Console.WriteLine(" Полный список студентов:");
-
-            for (int i = 0; i < students.Length; i++)
+            for (int i = 0; i < count; i++)
             {
-                if (students.GetValue(i) == null)
-                    break;
                 students[i].AllStudents();
             }
             Console.WriteLine("Список студентов, имеющих только отличные оценки:");
-            for (int i = 0; i < students.Length; i++)
+            for (int i = 0; i < count; i++)
             {
-                if (students.GetValue(i) == null)
-                    break;
                 students[i].ExcellentStudents();
             }
             Console.WriteLine("Cписок студентов, имеющих хотя бы одну неудовлетворительную оценку:");
-            for (int i = 0; i < students.Length; i++)
+            for (int i = 0; i < count; i++)
             {
-                if (students.GetValue(i) == null)
-                    break;
                 students[i].BadStudents();
             }
         }
@@ -36,40 +28,42 @@ namespace LabWorkSeven
     class Student
     {
         string fullName;
-        string score1;
-        string score2;
+        int score1;
+        int score2;
 
-
-        public Student(string fullName, string score1, string score2)
+        public Student(string fullName, int score1, int score2)
         {
             this.fullName = fullName;
             this.score1 = score1;
             this.score2 = score2;
         }
-        public static void NewStudent(Student[] array)
+        public static int NewStudent(Student[] array)
         {
-            for (int num = 0; num < array.Length; num++)
+            var countOfStudents = 0;
+            for (int i = 0; i < array.Length; i++)
             {
-                Console.WriteLine($"Введите ФИО {num + 1}-го студента: ");
+                Console.WriteLine($"Введите ФИО {i + 1}-го студента: ");
                 string fullName = Console.ReadLine();
-                Console.WriteLine($"Введите оценку {num + 1}-го студента по программированию: ");
-                string score1 = (Console.ReadLine());
-                Console.WriteLine($"Введите оценку {num + 1}-го студента по математике: ");
-                string score2 = (Console.ReadLine());
-                array[num] = new Student(fullName, score1, score2);
+                Console.WriteLine($"Введите оценку {i + 1}-го студента по программированию: ");
+                int score1 = (Convert.ToInt32(Console.ReadLine()));
+                Console.WriteLine($"Введите оценку {i + 1}-го студента по математике: ");
+                int score2 = (Convert.ToInt32(Console.ReadLine()));
+                array[i] = new Student(fullName, score1, score2);
+                countOfStudents++;
                 for (; ; )
                 {
-                    if (num + 1 == array.Length)
+                    if (countOfStudents == array.Length)
                     { break; }
                     Console.WriteLine("Ввести данные еще одного студента? (Enter - да/Escape - нет)");
                     break;
                 }
-                if (num + 1 == array.Length)
+                if (countOfStudents == array.Length)
                 { break; }
                 ConsoleKeyInfo key = Console.ReadKey();
                 if (key.Key == ConsoleKey.Escape)
                 { break; }
             }
+            return countOfStudents;
         }
         public void AllStudents()
         {
@@ -77,8 +71,8 @@ namespace LabWorkSeven
         }
         public void ExcellentStudents()
         {
-            var intscore1 = int.Parse(score1);
-            var intscore2 = int.Parse(score2);
+            var intscore1 = score1;
+            var intscore2 = score2;
             if (intscore1 == 5 && intscore2 == 5)
             {
                 Console.WriteLine($"Студент {fullName.ToUpper()}");
@@ -86,8 +80,8 @@ namespace LabWorkSeven
         }
         public void BadStudents()
         {
-            var intscore1 = int.Parse(score1);
-            var intscore2 = int.Parse(score2);
+            var intscore1 = score1;
+            var intscore2 = score2;
             if (intscore1 <= 2 || intscore2 <= 2)
             {
                 Console.WriteLine($"Студент {fullName.ToUpper()}");
